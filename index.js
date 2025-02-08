de53e5e64d458c3f58a32446567436bd6210efcf
assia <assiabenmohamed>
2025-02-08 08:45:01 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function menu(){
+    let element=document.getElementById("menuphone")
+    console.log(element)
+
+    element.classList.toggle("max-sm:hidden")
+    element.classList.toggle("max-sm:block")
+    console.log(element)
+
+
+
+//    if(element.style.display=="none")
+//    { element.classList.remove("max-sm:hidden");
+//     element.classList.add("max-sm:block");}
+//    else
+//    {element.classList.remove("max-sm:block");
+//    element.classList.add("max-sm:hidden");}
+}
